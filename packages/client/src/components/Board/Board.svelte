7ad705d44f32662a73d3745cab6b04498fa40763
Teemu Koivisto <tkoivisto456@gmail.com>
2023-01-12 00:37:14 +0200

    fix(client): center board

--- packages/client/src/components/Board/Board.svelte
+++ packages/client/src/components/Board/Board.svelte
@@ -15,19 +15,21 @@
   }
 </script>
 
-<div class="board-padding relative z-10 mt-8">
-  <div class="max-w-3xl relative">
-    <div class="w-full" style="padding-top: 100%" />
-  </div>
-  <div class="board-padding absolute inset-0 flex justify-center">
-    <div class="bg-white shadow-2xl rounded-xl w-full h-full max-w-3xl flex flex-col">
-      {#each Array($gridSize) as _, y}
-        <div class="row flex">
-          {#each Array($gridSize) as _, x}
-            <Cell class={`${y < $gridSize - 1 ? 'border-b' : ''}`} {y} {x} {handleCellClick} />
-          {/each}
-        </div>
-      {/each}
+<div class="h-screen flex justify-center items-center">
+  <div class="w-full relative z-10">
+    <div class="max-w-3xl relative">
+      <div class="w-full" style="padding-top: 100%" />
+    </div>
+    <div class="absolute inset-0 flex justify-center">
+      <div class="bg-white shadow-2xl rounded-xl w-full h-full max-w-3xl max-h-3xl flex flex-col">
+        {#each Array($gridSize) as _, y}
+          <div class="row flex">
+            {#each Array($gridSize) as _, x}
+              <Cell class={`${y < $gridSize - 1 ? 'border-b' : ''}`} {y} {x} {handleCellClick} />
+            {/each}
+          </div>
+        {/each}
+      </div>
     </div>
   </div>
 </div>
