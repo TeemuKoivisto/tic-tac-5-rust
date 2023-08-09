21c06a62ad93aa00d5d718af68882d986696ead0
Teemu Koivisto <tkoivisto456@gmail.com>
2023-08-09 21:40:36 +0300

    perf: make ai little bit faster

--- packages/ai/src/board2.ts
+++ packages/ai/src/board2.ts
@@ -24,7 +24,6 @@ export class Board {
   inRow = 3
   cells: number[] = []
   available = 3 * 3
-  available_moves: [number, number][] = []
 
   constructor(opts?: BoardOptions, previous?: Board) {
     this.size = previous?.size ?? opts?.gridSize ?? this.size
@@ -41,11 +40,14 @@ export class Board {
       }
     }
     this.cells = cells
-    this.available_moves = cells.filter(c => getOwner(c) === 0).map(c => getCellCoords(c))
   }
 
   is_within_board(x: number, y: number) {
     return x >= 0 && y >= 0 && x < this.size && y < this.size
+  }
+
+  get_cell_idx(c: number) {
+    return 0b111111 & (c + (0b111111 & ((c >> 6) * this.size)))
   }
 
   get_cell_at(x: number, y: number) {
@@ -56,23 +58,13 @@ export class Board {
     return getCellValue(this.cells[x + y * this.size])
   }
 
-  // set_cell_owner(cell: number, player: number) {
-  //   this.cells[x + y * this.size] = setOwner(this.cells[x + y * this.size], player)
-  //   if (player !== 0) {
-  //     this.available -= 1
-  //   } else {
-  //     this.available += 1
-  //   }
-  // }
-
-  set_cell_owner(x: number, y: number, player: number) {
-    this.cells[x + y * this.size] = setOwner(this.cells[x + y * this.size], player)
+  set_cell_owner(c: number, player: number) {
+    const idx = this.get_cell_idx(c)
+    this.cells[idx] = setOwner(this.cells[idx], player)
     if (player !== 0) {
       this.available -= 1
-      this.available_moves = this.available_moves.filter(([xx, yy]) => xx !== x || yy !== y)
     } else {
       this.available += 1
-      this.available_moves.push(getCellCoords(this.cells[x + y * this.size]))
     }
   }
 
@@ -137,55 +129,48 @@ export class Board {
     return undefined
   }
 
-  get_adjacent_cells(x: number, y: number, player: number, dir: Adjacency) {
-    const adjacent: [number, number, number][] = []
+  get_adjacent_cells(c: number, player: number, dir: Adjacency) {
+    const adjacent: number[] = []
     let topside = true
-    let now_x = x
-    let now_y = y
+    let now_x = 0b111111 & c
+    let now_y = 0b111111 & (c >> 6)
     let iters = 0
     let cell: number | undefined
-    let c: [number, number, number] | undefined
     while (true) {
       cell = this.get_adjacent_in_direction(now_x, now_y, dir, topside)
-      if (iters > 20) {
-        throw Error('infinite loop')
-      }
-      c = cell !== undefined ? getCellCoordsOwner(cell) : undefined
-      if (c && c[2] === player) {
+      if (cell !== undefined && getOwner(cell) === player) {
         adjacent.push(c)
-        now_x = c[0]
-        now_y = c[1]
+        now_x = 0b111111 & cell
+        now_y = 0b111111 & (cell >> 6)
       } else if (topside) {
         topside = false
-        now_x = x
-        now_y = y
+        now_x = 0b111111 & c
+        now_y = 0b111111 & (c >> 6)
       } else {
         break
+      }
+      if (iters > 20) {
+        throw Error('infinite loop')
       }
       iters += 1
     }
     return adjacent
   }
 
-  update_cell_owner(x: number, y: number, player: number) {
-    this.set_cell_owner(x, y, player)
+  update_cell_owner(c: number, player: number) {
+    this.set_cell_owner(c, player)
     let bestInRow = 0
+    let idx = 0
     for (let i = 0; i < 4; i += 1) {
       const dir = i as Adjacency
-      const cells = this.get_adjacent_cells(x, y, player, dir)
+      const cells = this.get_adjacent_cells(c, player, dir)
       const adjacent_count = cells.length + 1
       for (const c of cells) {
-        this.cells[c[0] + c[1] * this.size] = setDirAdjacency(
-          this.cells[c[0] + c[1] * this.size],
-          dir,
-          adjacent_count
-        )
+        idx = this.get_cell_idx(c)
+        this.cells[idx] = setDirAdjacency(this.cells[idx], dir, adjacent_count)
       }
-      this.cells[x + y * this.size] = setDirAdjacency(
-        this.cells[x + y * this.size],
-        dir,
-        adjacent_count
-      )
+      idx = this.get_cell_idx(c)
+      this.cells[idx] = setDirAdjacency(this.cells[idx], dir, adjacent_count)
       if (adjacent_count > bestInRow) {
         bestInRow = adjacent_count
       }
@@ -197,16 +182,7 @@ export class Board {
     return this.available === 0
   }
 
-  // check_win_at(x: number, y: number) {
-  //   const cell = this.get_cell_at(x, y)
-  //   return Object.values(cell.adjacency).some(v => v === this.inRow)
-  // }
-
-  // check_win() {
-  //   return this.cells.find(c => Object.values(c.adjacency).some(v => v === this.inRow))
-  // }
-
-  // get_available_moves(): Cell[] {
-  //   return this.cells.filter(c => getOwner(c) === 0).map(c => getCellValue(c))
-  // }
+  get_available_moves(): number[] {
+    return this.cells.filter(c => getOwner(c) === 0)
+  }
 }
--- packages/ai/src/game.ts
+++ packages/ai/src/game.ts
@@ -1,12 +1,12 @@
 import { get, writable } from 'svelte/store'
-import { Cell } from './cell'
+import { Cell, getCellValue } from './cell'
 // import { Board } from './board'
 import { Board } from './board2'
 
 export const board = writable<Board>(new Board())
 export const gridSize = writable(3)
 export const player = writable<'x' | 'o'>('x')
-export const searchDepth = writable(6)
+export const searchDepth = writable(7)
 export const gameStatus = writable<'running' | 'x-won' | 'o-won' | 'tie'>('running')
 
 let iterations = 0
@@ -26,7 +26,7 @@ interface Options {
 }
 
 function minimax(
-  coords: [number, number],
+  cell: number,
   board: Board,
   depth: number,
   isMaximizing: boolean,
@@ -36,8 +36,7 @@ function minimax(
   opts: Options
 ) {
   iterations += 1
-  // console.log('cell', selectedCell)
-  if (board.update_cell_owner(coords[0], coords[1], player)) {
+  if (board.update_cell_owner(cell, player)) {
     return opts.humanPlayer === player ? -1000 - depth : 1000 + depth
   } else if (board.is_full()) {
     return opts.humanPlayer === player ? -100 - depth : 100 + depth
@@ -47,24 +46,24 @@ function minimax(
   let value: number
   if (isMaximizing) {
     value = Number.NEGATIVE_INFINITY
-    board.available_moves.some(c => {
+    board.get_available_moves().some(c => {
       value = Math.max(
         value,
         minimax(c, board, depth - 1, false, alpha, beta, player === 1 ? 2 : 1, opts)
       )
       alpha = Math.max(alpha, value)
-      board.update_cell_owner(c[0], c[1], 0)
+      board.update_cell_owner(c, 0)
       return beta <= alpha
     })
   } else {
     value = Number.POSITIVE_INFINITY
-    board.available_moves.some(c => {
+    board.get_available_moves().some(c => {
       value = Math.min(
         value,
         Math.min(value, minimax(c, board, depth - 1, true, alpha, beta, player === 1 ? 2 : 1, opts))
       )
       beta = Math.min(beta, value)
-      board.update_cell_owner(c[0], c[1], 0)
+      board.update_cell_owner(c, 0)
       return beta <= alpha
     })
   }
@@ -87,13 +86,14 @@ export const gameActions = {
   },
   playerSelectCell(x: number, y: number) {
     const b = get(board)
+    const c = b.get_cell_at(x, y)
     const cell = b.get_cell_value_at(x, y)
     if (cell.owner !== 0) {
       return
     }
     const playerNumber = get(player) === 'x' ? 1 : 2
     let ended = true
-    if (b.update_cell_owner(cell.x, cell.y, playerNumber)) {
+    if (b.update_cell_owner(c, playerNumber)) {
       gameStatus.set(playerNumber === 1 ? 'x-won' : 'o-won')
     } else if (b.is_full()) {
       gameStatus.set('tie')
@@ -108,7 +108,7 @@ export const gameActions = {
   },
   evaluateAiMove() {
     const b = get(board)
-    let aiMove: [number, number] | undefined
+    let chosenCell: number | undefined
     let bestValue = Number.NEGATIVE_INFINITY
     const aiNumber = get(player) === 'x' ? 2 : 1
     const opts = {
@@ -118,15 +118,15 @@ export const gameActions = {
     }
     iterations = 0
     const t0 = performance.now()
-    b.available_moves.forEach(c => {
+    b.get_available_moves().forEach(c => {
       const value = minimax(c, b, get(searchDepth), false, -Infinity, Infinity, aiNumber, opts)
-      b.update_cell_owner(c[0], c[1], 0)
+      b.update_cell_owner(c, 0)
       if (value > bestValue) {
-        aiMove = c
+        chosenCell = c
         bestValue = value
       }
     })
-    if (!aiMove) {
+    if (!chosenCell) {
       throw Error('no ai move found')
     }
     const t1 = performance.now()
@@ -135,8 +135,9 @@ export const gameActions = {
         6
       )} per iteration`
     )
-    console.log(`best: ${aiMove[0]} ${aiMove[1]} ${bestValue} at iterations ${iterations} \n`)
-    if (b.update_cell_owner(aiMove[0], aiMove[1], aiNumber)) {
+    const aiMove = getCellValue(chosenCell)
+    console.log(`best: ${aiMove.x} ${aiMove.x} ${bestValue} at iterations ${iterations} \n`)
+    if (b.update_cell_owner(chosenCell, aiNumber)) {
       gameStatus.set(aiNumber === 2 ? 'o-won' : 'x-won')
     } else if (b.is_full()) {
       gameStatus.set('tie')
